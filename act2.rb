9310f39f68da763b03b78803bac3bdada4fd1722
Maria Kennedy <mariakingsbury1@gmail.com>
2024-03-09 19:13:59 -0600

    Initial commit. P1 complete.

---
+++ act2.rb
@@ -0,0 +1,68 @@
+############################################
+# Problem 1: Method Calls Without Parentheses
+
+def tag(html_tag, content)
+  "<#{html_tag}>#{content}</#{html_tag}>"
+end
+
+# P1 Test Cases
+puts tag :p, "This is a paragraph"
+puts tag :h1, "This is a header"
+
+
+
+# Problem 2: Blocks and Iteration
+
+
+# Problem 3: Exception Handling
+
+
+
+# Problem 4: File I/O
+
+
+
+# Problem 5: Custom Exceptions
+
+
+
+# Problem 6: Symbols as Hash Keys
+
+
+
+# Problem 7: Enumerable select Method
+
+
+
+# Problem 8: The unless Keyword
+
+
+
+# Problem 9: Global Variables
+
+
+
+# Problem 10: Iterating with times
+
+
+
+# Problem 11: Ranges and Loops
+
+
+
+# Problem 12: Reading Specific Lines from a File
+
+
+
+# Problem 13: Command-line Arguments
+
+
+
+# Problem 14: Using map with a Block
+
+
+
+# Problem 15: Handling ZeroDivisionError
+
+
+
